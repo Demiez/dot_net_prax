4dc1934aeeaf4500c137c03bd36a87ec67a18e38
Demiez <dmg.petrenko@gmail.com>
2019-12-07 22:00:14 +0200

    Done 01, 02 tasks

---
+++ task01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01
+{
+    /*
+     01 Табл. 1.1 - Задачи для индивидуального выполнения 1
+     Предприниматель 1 марта открыл счет в банке, вложив 1000 руб. Через каждый месяц размер вклада увеличивается на 2% от имеющейся суммы. Определить  прирост суммы вклада за первый, второй, ..., десятый месяц.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateIncome(1000);
+            Console.ReadLine();
+        }
+
+        static void CalculateIncome(int investment)
+        {
+            double income = investment;
+            Console.WriteLine("Ваш первоначальный доход: {0}", income);
+
+            for (int i = 0; i < 10; i++)
+            {
+                income += income * 0.02;
+                Console.WriteLine("Теперь ваш доход: {0}", Math.Round(income, 2));
+            }
+        }
+    }
+}
---
+++ task02/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    /*
+    02 Табл. 2.1 - Вычислительные задачи 1
+    low - CheckNumber01
+    Дано натуральное число. Определить, есть ли в нем цифра а.
+    mid - CheckNumber02
+    Дано натуральное число. Определить, верно ли, что произведение его цифр меньше а, а само число делится на b?
+    high - CheckNumber03
+    Даны натуральные числа m и n. Получить все натуральные числа, меньшие n, квадрат суммы цифр которых равен m.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Проверям числа: ***");
+            // Любой аргумент можно ввести из консоли используя блок кода, для ускорения аргументы прописаны
+            /* Console.Write("number = ");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number);
+            */
+
+            CheckNumber01(345, 3);
+            CheckNumber02(34522456, 34523, 2);
+            CheckNumber03(144, 94);
+
+            Console.ReadLine();
+        }
+
+        static void CheckNumber01(int target, int a)
+        {
+            string stringNumber = target.ToString();
+            if (stringNumber.IndexOf($"{a}") != -1)
+            {
+                Console.WriteLine($"Ваше число: {stringNumber} содержит {a}");
+                return;
+            }
+            Console.WriteLine($"Ваше число: {stringNumber} не содержит {a}");
+        }
+
+        static void CheckNumber02(int target, int a, int b)
+        {
+            int[] resultArray = SplitIntoNumbers(target);
+            int multiplyResult = 1;
+            foreach (int element in resultArray)
+            {
+                multiplyResult *= element;
+            }
+            var result = (multiplyResult < a && multiplyResult % b == 0) ?
+                $"Произведение цифр числа {target} равно {multiplyResult} ДА (меньше {a} и делится на {b})" :
+                $"Произведение цифр числа {target} равно {multiplyResult} НЕТ (меньше {a} и делится на {b})";
+            Console.WriteLine(result);
+        }
+
+        static int[] SplitIntoNumbers(int number)
+        {
+            List<int> numbersList = new List<int>();
+            int basicDivider = 1, result = 0;
+
+            do
+            {
+                result = (number / basicDivider) % 10;
+                basicDivider *= 10;
+                numbersList.Add(result);
+
+            } while (result != 0);
+
+            int[] resultArray = numbersList.ToArray();
+            Array.Resize(ref resultArray, resultArray.Length - 1);
+
+            return resultArray;
+        }
+
+        static void CheckNumber03(int m, int n)
+        {
+            Console.WriteLine($"Натуральные числа меньше {n}, квадрат суммы их цифр равен {m}:");
+            for (int i = 1; i < n; i++)
+            {
+                int sum = 0, temp = i;
+                do
+                {
+                    sum += temp % 10;
+                    temp /= 10;
+                }
+                while (temp != 0);
+
+                if (m == Math.Pow(sum, 2))
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
