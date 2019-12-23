9d02dc09ecd53963e08d2414c65773c455bfe251
Demiez <dmg.petrenko@gmail.com>
2019-12-23 23:23:28 +0200

    fixed 1st and 3rd tasks

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task01
 {
@@ -21,12 +17,12 @@ namespace task01
         static void CalculateIncome(int investment)
         {
             double income = investment;
-            Console.WriteLine("Ваш первоначальный доход: {0}", income);
+            Console.WriteLine("Ваш первоначальный вклад: {0}", income);
 
             for (int i = 0; i < 10; i++)
             {
                 income += income * 0.02;
-                Console.WriteLine("Теперь ваш доход: {0}", Math.Round(income, 2));
+                Console.WriteLine("Теперь ваш доход: {0}", Math.Round(income - investment, 2));
             }
         }
     }
--- task03/Program.cs
+++ task03/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task03
 {
@@ -17,44 +13,54 @@ namespace task03
     {
         static void Main(string[] args)
         {
-            int[] array = { 2, 3, 4, -5, 1 };
-            double[] arrayWithPair = { 2, 3, 4, 2, -5.7, 4, 5, 1, 5.7, 3, 1 };
+            double[] array = { 2, 3, 4, -5, 1, 2};
+            // double[] arrayWithPair = { 2, 3, 4, 2, -5.7, 4, 5, 1, 5.7, 3, 1 };
+
+            double[] checkArray = { 2, -2, -2, 2, -3, -3, 3, 3, 5.7, 1 };
 
             FindAverageAndMin(array);
-            FindPairAndSwap(arrayWithPair);
+            FindPairAndSwap(checkArray);
 
             Console.ReadLine();
         }
 
-        static void FindAverageAndMin(int[] numbers)
+        static void FindAverageAndMin(double[] numbers)
         {
-            int min = numbers[0], sum = 0;
-            foreach (int elem in numbers)
+            double min = numbers[0], sum = 0;
+            foreach (double elem in numbers)
             {
                 sum += elem;
                 if (min > elem) min = elem;
             }
-            int avSum = sum / numbers.Length;
+            double avSum = sum / numbers.Length;
             Console.WriteLine("Среднее арифметическое массива: {0}, минимальное число: {1}", avSum, min);
         }
 
         static void FindPairAndSwap(double[] array)
         {
+            object[] usedArray = new object[array.Length]; // Создаем зеркальный массив подобной длины, но тип данных объект, тк может принимать любые значения
+            double pass = double.NaN; // добавим значение плейсхолдер, чтобы отметить измененные ячейки, нан не равен нан;
+
             Console.WriteLine("До перестановки: [{0}]", string.Join(", ", array));
             double El1 = 0, El2 = 0;
             for (var i = 0; i < array.Length; i++)
             {
                 for (var j = i + 1; j < array.Length; j++)
                 {
-                    if (Math.Abs(array[i]) == Math.Abs(array[j]) && (array[i] < 0 || array[j] < 0))
+                    if (Math.Abs(array[i]) == Math.Abs(array[j]) && ((array[i] < 0 && array[j] > 0) || (array[i] > 0 && array[j] < 0)))
                     {
                         El1 = array[i];
                         El2 = array[j];
-                        array[i] = El2;
-                        array[j] = El1;
+                        usedArray[i] = El2; // меняем местами и заносим в зеркальный массив
+                        usedArray[j] = El1;
+                        array[i] = pass;
+                        array[j] = pass;
+                        break;
                     }
                 }
+                usedArray[i] = usedArray[i] == null ? array[i] : usedArray[i];
             }
+            array = Array.ConvertAll<object, double>(usedArray, x => (double)x);
             Console.WriteLine("После перестановки: [{0}]", string.Join(", ", array));
         }
     }
